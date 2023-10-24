e770e955f282c3b304028b8a14eba93ab3e1a6de
Brian Gomez <briannoemi2004@gmail.com>
2023-10-24 16:21:40 +0200

    Seguida ej 5 de practica

---
+++ Desarrollo Web en entorno Servidor/Tema 2/03_arquitectura_web_wsgi_asgi/ninja_trainings/01_hello_http_world/web/src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primera Web</title>
+</head>
+
+<body>
+    <?php 
+   //echo "Hola " . $_GET["nombre"] . ", tu edad es " . $_GET["edad"] . " años";
+
+    echo $_GET["n1"] + $_GET["n2"];
+
+
+    ?>
+</body>
+
+</html>
